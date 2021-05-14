b43e7c377f465db84a3e731cc987b488199e884d
“Faraz <ifarazir@gmail.com>
2021-05-14 23:51:25 +0430

    add random quiz

--- app/Http/Controllers/User/UserQQuestionController.php
+++ app/Http/Controllers/User/UserQQuestionController.php
@@ -16,14 +16,23 @@ class UserQQuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function RandomQuiz(Request $request)
+     public function RandomQuiz(Request $request)
     {
-
-        if(is_null($request->limit))
+        if (is_null($request->limit)) {
             return response()->json(["status" => "failed", "message" => "Failed! Limit Question Required!"], 200);
-        $questions = QQuestion::where('lesson_id', $request->lesson_id)->inRandomOrder()->limit($request->limit)->get();
-        if (!is_null($questions)) {
-            return response()->json(["status" => "success", "data" => $questions->makeHidden(['created_at', 'updated_at'])], 200);
+        }
+
+        $request['lessons_id'] = explode(",", $request['lessons_id']);
+
+        $questions = QQuestion::whereIn('lesson_id', [2, 3])->get()->toArray();
+
+        if (count($questions) < $request->limit) {
+            return response()->json(["status" => "failed", "message" => "Failed! Limit Question is bigger than question count!"], 200);
+        }
+        $items = Arr::random($questions, $request->limit);
+
+        if (!is_null($items)) {
+            return response()->json(["status" => "success", "data" => $items], 200);
         } else {
             return response()->json(["status" => "failed", "message" => "Failed! no Question found"], 200);
         }
