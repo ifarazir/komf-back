f32beac5b5c956fff17ddb6941738d38f9994cd9
“Faraz <ifarazir@gmail.com>
2021-05-15 20:29:57 +0430

    Update UserQQuestionController.php

--- app/Http/Controllers/User/UserQQuestionController.php
+++ app/Http/Controllers/User/UserQQuestionController.php
@@ -24,8 +24,7 @@ class UserQQuestionController extends Controller
 
         $request['lessons_id'] = explode(",", $request['lessons_id']);
 
-        $questions = QQuestion::whereIn('lesson_id', $request['lessons_id'])->get()->toArray();
-        dd($questions);
+        $questions = QQuestion::whereIn('lesson_id', $request['lessons_id'])->get();
         if (count($questions) < $request->limit) {
             return response()->json(["status" => "failed", "message" => "Failed! Limit Question is bigger than question count!"], 200);
         }
